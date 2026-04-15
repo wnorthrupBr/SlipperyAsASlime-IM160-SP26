fec990811e9975f853775fd149cf2027e5f6061a
wnorthrupBr <wnorthrup@mail.bradley.edu>
2026-04-15 15:28:49 -0500

    replaced walls in level 1

--- Assets/Scripts/ExitController.cs
+++ Assets/Scripts/ExitController.cs
@@ -1,8 +1,20 @@
+/*****************************************************************************
+// File Name : ExitController.cs
+// Author : Will Northrup
+// Creation Date : 3/24/2026
+//
+// Brief Description : This is a script that takes the player to the next
+// scene when they make contact with the exit game object.
+*****************************************************************************/
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExitController : MonoBehaviour
 {
+    /// <summary>
+    /// loads next scene on contact with exit
+    /// </summary>
+    /// <param name="collidingObject"></param>
     private void OnTriggerEnter(Collider collidingObject)
     {
         if (collidingObject.gameObject.name == "Player")
